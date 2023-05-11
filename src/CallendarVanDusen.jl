d2fc4abd3d464ea447a14e29965ece755d6754cf
KronosTheLate <densbal@live.no>
2023-05-11 19:11:24 +0200

    Files generated by PkgTemplates
    PkgTemplates version: 0.7.34

---
+++ src/CallendarVanDusen.jl
@@ -0,0 +1,5 @@
+module CallendarVanDusen
+
+# Write your package code here.
+
+end
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using CallendarVanDusen
+using Test
+
+@testset "CallendarVanDusen.jl" begin
+    # Write your tests here.
+end
